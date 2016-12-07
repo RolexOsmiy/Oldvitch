a416d2b66f8a264873ce54266f1f5e612137dc56
RolexOsmiy <wow.rolex@yandex.by>
2016-12-07 17:39:06 +0300

    first  commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+    Animator anim;
+    public bool grounded = true;
+    public float walkSpeed = 4;
+    public float sprintSpeed = 8;
+    public float rotateSpeed = 200;
+    public float jumpPower = 10;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * walkSpeed * Time.deltaTime);
+            anim.SetFloat("speed", 0.4f);
+        }
+        else
+        {
+            anim.SetFloat("speed", 0);
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift) && grounded == false)
+        {
+            transform.Translate(Vector3.forward * sprintSpeed * Time.deltaTime);
+            anim.SetFloat("speed", 0.8f);
+        }
+        else
+        {
+            if (Input.GetKey(KeyCode.S))
+            {
+                transform.Translate(-Vector3.forward * walkSpeed * Time.deltaTime);
+                anim.SetFloat("speed", 0.4f);
+            }
+        }      
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.down * rotateSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime);
+        }
+        if (!grounded && GetComponent<Rigidbody>().velocity.y == 0)
+        {
+            grounded = true;
+        }
+        if (Input.GetKey(KeyCode.Space) && grounded == true)
+        {
+            GetComponent<Rigidbody>().AddRelativeForce(new Vector3(0f, jumpPower, 0f), ForceMode.Impulse);
+            grounded = false;
+            anim.SetBool("jump", true);
+            anim.SetBool("jump", false);
+        }
+    }
+}
+    
