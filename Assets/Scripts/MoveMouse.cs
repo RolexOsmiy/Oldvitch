2801680a00f814b1c93713cc1e1dc58ee292cbc8
RolexOsmiy <wow.rolex@yandex.by>
2016-12-11 09:34:42 +0200

    add mouse control

--- Assets/Scripts/MoveMouse.cs
+++ Assets/Scripts/MoveMouse.cs
@@ -3,24 +3,85 @@ using System.Collections;
 
 public class MoveMouse : MonoBehaviour
 {
-    public GameObject cam;
-    public float cursorLastPosX;
-    public float cursorLastPosY;
+	public Transform target;
+	public float distance = 6.0f;
+	public float maxDistance = 6.0f;
+	float tmpDistance = 6.0f;
 
-    void Start()
-    {
-        cam = GameObject.Find("Camera").gameObject;
-    }
+	public float rotateSpeed = 10;
 
-    void Update()
-    {
+	public float bufferup = 1.5f;
+	public float bufferright = 0.75f;
 
-    }
+	public float xSpeed = 250.0f;
+	public float ySpeed = 120.0f;
 
-    void FixedUpdate()
-    {
-        cam.transform.Rotate(cursorLastPosY - Input.mousePosition.y, Input.mousePosition.x - cursorLastPosX, 0);
-        cursorLastPosX = Input.mousePosition.x;
-        cursorLastPosY = Input.mousePosition.y;
-    }
+	public float yMinLimit = -50f;
+	public float yMaxLimit = 50f;
+
+	private float x = 0.0f;
+	private float y = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		Cursor.lockState = CursorLockMode.Locked;
+		Vector3 angles = transform.eulerAngles;
+		x = angles.y;
+		y = angles.x;
+
+		// Make the rigid body not change rotation
+		if (GetComponent<Rigidbody>())
+			GetComponent<Rigidbody>().freezeRotation = true;
+	}
+
+	void Update ()
+	{
+		if(Input.GetMouseButton(1))
+		{
+			distance -= 20*Time.deltaTime;
+		}
+		else
+		{
+			if(distance <= tmpDistance)
+			{
+				distance += 20*Time.deltaTime;
+			}
+		}
+	}
+
+	// Update is called once per frame
+	void LateUpdate () 
+	{
+		if (target)
+		{
+			//distance -= .5f * Input.mouseScrollDelta.y;
+			if (distance < 3)
+			{
+				distance = 3;
+			}
+			if (distance > maxDistance)
+			{
+				distance = maxDistance;
+			}
+			x += Input.GetAxis("Mouse X") * xSpeed * 0.02f;
+			y -= Input.GetAxis("Mouse Y") * ySpeed * 0.02f;
+
+			y = ClampAngle(y, yMinLimit, yMaxLimit);
+
+			Quaternion rotation = Quaternion.Euler(y, x, 0);
+			Vector3 position = rotation * new Vector3(bufferright, 0.0f, -distance) + target.position + new Vector3(0.0f, bufferup, 0.0f);
+
+			transform.rotation = rotation;
+			transform.position = position;
+		}
+	}
+
+	float ClampAngle(float angle, float min, float max)
+	{
+		if (angle < -360)
+			angle += 360;
+		if (angle > 360)
+			angle -= 360;
+		return Mathf.Clamp(angle, min, max);
+	}
 }
